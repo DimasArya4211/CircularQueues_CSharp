acb833313485c5a37278f705c92fdf5db8bba18f
ACER <ACER@LAPTOP-PJA37DT8>
2022-12-14 17:05:47 +0700

    Add project files.

---
+++ CircularQueues_CSharp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircularQueues_CSharp
+{
+    class Queues
+    {
+        int FRONT, REAR, max = 5;
+        int[] queue_array = new int[5];
+        public Queues()
+        {
+            
+        }
+    }
+}
